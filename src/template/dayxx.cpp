d29a211bd1e5c4edddc5615416b250d654ad565e
Totto16 <tobiausgais@gmail.com>
2023-12-01 18:39:54 +0100

    fix template folder

---
+++ src/template/dayxx.cpp
@@ -0,0 +1,31 @@
+
+
+#include "helpers/base.hpp"
+#include "helpers/utility.hpp"
+
+struct AoCDayXX : AoCDay {
+
+  AoCDayXX() : AoCDay(1) {
+    //
+  }
+
+  ResultType solvePart1(std::string input,
+                        [[maybe_unused]] const bool is_example) const override {
+
+    ResultType result = 0;
+
+    return result;
+  }
+
+  ResultType solvePart2(std::string input,
+                        [[maybe_unused]] const bool is_example) const override {
+
+    ResultType result = 0;
+
+    return result;
+  }
+};
+
+DayRegister<AoCDayXX> dayxx{Input::SameInput("input.txt") >>
+                            Input::ExampleInput("example_1.txt", -1) >>
+                            Input::ExampleInput("example_2.txt", -1)};
