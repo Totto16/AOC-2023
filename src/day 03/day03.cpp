26e39888e1c3760ee0b57de459355b95519c3d9a
Totto16 <tobiausgais@gmail.com>
2023-12-03 14:36:56 +0100

    add solution for part 2

--- src/day 03/day03.cpp
+++ src/day 03/day03.cpp
@@ -32,7 +32,7 @@ std::optional<std::uint64_t> get_number(const std::string &inp) {
   return result;
 }
 
-enum class CharClass { Period, Symbol };
+enum class CharClass { Period, Symbol, Gear };
 
 // helper type for the visitor
 template <class... Ts> struct Overloaded : Ts... {
@@ -185,11 +185,140 @@ struct AoCDay03 : AoCDay {
 
     ResultType result = 0;
 
-    result += input.size();
+    std::vector<std::vector<VectorContent>> rows{};
+
+    for (const auto &line : splitByNewLine(input)) {
+
+      std::size_t pos = 0;
+      std::vector<VectorContent> inner_vec{};
+
+      while (pos < line.size()) {
+        auto num = get_number(line.substr(pos));
+        if (num.has_value()) {
+
+          auto value = std::make_shared<std::uint64_t>(num.value());
+
+          const auto num_size = std::to_string(num.value()).size();
+          for (std::size_t i = 0; i < num_size; ++i) {
+            inner_vec.emplace_back(value);
+          }
+
+          pos += num_size;
+        } else {
+          const char given = line.at(pos);
+          if (given == '.') {
+            inner_vec.emplace_back(CharClass::Period);
+          } else if (given == '*') {
+            inner_vec.emplace_back(CharClass::Gear);
+          } else {
+            inner_vec.emplace_back(CharClass::Symbol);
+          }
+          ++pos;
+        }
+      }
+
+      rows.push_back(inner_vec);
+    }
+
+    auto is_gear = [](const VectorContent &cont) {
+      return std::visit(
+          Overloaded{
+              [](const CharClass &c) { return c == CharClass::Gear; },
+              [](const std::shared_ptr<std::uint64_t> &) { return false; }},
+          cont);
+    };
+
+    auto get_number_at = [&rows](const std::size_t i, const std::size_t j)
+        -> std::optional<std::shared_ptr<std::uint64_t>> {
+      return std::visit(
+          Overloaded{[](const CharClass &)
+                         -> std::optional<std::shared_ptr<std::uint64_t>> {
+                       return std::nullopt;
+                     },
+                     [](const std::shared_ptr<std::uint64_t> &s)
+                         -> std::optional<std::shared_ptr<std::uint64_t>> {
+                       return s;
+                     }},
+          rows.at(i).at(j));
+    };
+
+    auto get_pos = [&rows](std::size_t i, std::size_t j) {
+      std::vector<std::pair<std ::size_t, std::size_t>> result{};
+
+      const auto size_i = rows.size() - 1;
+      const auto size_j = rows.at(i).size() - 1;
+
+      if (i > 0) {
+        if (j > 0) {
+          result.emplace_back(i - 1, j - 1);
+        }
+
+        result.emplace_back(i - 1, j);
+        if (j < size_j) {
+          result.emplace_back(i - 1, j + 1);
+        }
+      }
+
+      if (i < size_i) {
+        if (j > 0) {
+          result.emplace_back(i + 1, j - 1);
+        }
+        result.emplace_back(i + 1, j);
+        if (j < size_j) {
+          result.emplace_back(i + 1, j + 1);
+        }
+      }
+
+      if (j > 0) {
+        result.emplace_back(i, j - 1);
+      }
+
+      if (j < size_j) {
+        result.emplace_back(i, j + 1);
+      }
+
+      return result;
+    };
+
+    for (std::size_t i = 0; i < rows.size(); ++i) {
+
+      const auto columns = rows.at(i);
+
+      for (std::size_t j = 0; j < columns.size(); ++j) {
+
+        const auto contained = columns.at(j);
+
+        if (is_gear(contained)) {
+          std::vector<std::shared_ptr<std::uint64_t>> added{};
+          for (const auto &[pos_i, pos_j] : get_pos(i, j)) {
+
+            auto res = get_number_at(pos_i, pos_j);
+            if (res.has_value()) {
+              const auto res_val = res.value();
+              bool already_included = false;
+              for (const auto &inc : added) {
+                if (inc.get() == res_val.get()) {
+                  already_included = true;
+                }
+              }
+
+              if (!already_included) {
+                added.emplace_back(res_val);
+              }
+            }
+          }
+
+          if (added.size() == 2) {
+
+            result += (*added.at(0)) * (*added.at(1));
+          }
+        }
+      }
+    }
 
     return result;
   }
 };
 
 DayRegister<AoCDay03> day03{Input::SameInput("input.txt") >>
-                            Input::SameExample("example.txt", 4361, -1)};
+                            Input::SameExample("example.txt", 4361, 467835)};
