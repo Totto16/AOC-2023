a063538de90fff2171a4783b93530294d680644b
Totto16 <tobiausgais@gmail.com>
2023-12-09 20:43:48 +0100

    add day09 part 2

--- src/day 09/day09.cpp
+++ src/day 09/day09.cpp
@@ -94,10 +94,57 @@ struct AoCDay09 : AoCDay {
 
         ResultType result = 0;
 
-        result += input.size();
+        for (const auto& line : splitByNewLine(input)) {
+
+            const auto& start_numbers_vec = Day09::parse_numbers(line);
+            std::deque<ResultType> start_numbers{};
+            for (const auto& v : start_numbers_vec) {
+                start_numbers.push_back(v);
+            }
+
+            std::vector<std::deque<ResultType>> numbers{ start_numbers };
+            for (std::size_t i = 0; i < numbers.size(); ++i) {
+
+                const auto& current_numbers = numbers.back();
+
+
+                const auto have_all_0 = std::all_of(
+                        current_numbers.begin(), current_numbers.end(), [](const auto& number) { return number == 0; }
+                );
+                if (!have_all_0) {
+
+
+                    assert_greater<std::size_t>(
+                            current_numbers.size(), 0, "At least one element expected in current_numbers list!"
+                    );
+
+                    std::deque<ResultType> difference{};
+                    for (std::size_t j = 1; j < current_numbers.size(); ++j) {
+                        difference.push_back(current_numbers.at(j) - current_numbers.at(j - 1));
+                    }
+
+
+                    numbers.push_back(difference);
+                }
+            }
+
+            assert_greater<std::size_t>(numbers.size(), 1, "At least two element expected in numbers list!");
+
+            numbers.back().push_front(0);
+
+            for (std::size_t i = numbers.size() - 1; i != 0; --i) {
+
+                const auto new_element = numbers.at(i - 1).front() - numbers.at(i).front();
+
+                numbers.at(i - 1).push_front(new_element);
+            }
+
+            result += numbers.front().front();
+        }
+
 
         return result;
     }
 };
 
-DayRegister<AoCDay09> day09{ Input::SameInput("input.txt") >> Input::SameExample("example.txt", 114, -1) };
+DayRegister<AoCDay09> day09{ Input::SameInput("input.txt") >> Input::SameExample("example.txt", 114, 2) };
